e91a9f0b0a7021d66da32388fa6d726c0c0eb28e
jlap11 <jlap.11@hotmail.com>
2025-10-09 19:34:06 -0500

    feat: add babel-plugin-module-resolver for EAS Build path resolution + configure env vars

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,37 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+            '@/components': './components',
+            '@/constants': './constants',
+            '@/types': './types',
+            '@/utils': './utils',
+            '@/services': './services',
+            '@/hooks': './hooks',
+            '@/assets': './assets',
+            '@/dto': './dto',
+            '@/models': './models',
+            '@/environment': './environment',
+            '@/contexts': './contexts',
+          },
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.js',
+            '.jsx',
+            '.json',
+            '.tsx',
+            '.ts',
+            '.native.js',
+          ],
+        },
+      ],
+    ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -2,11 +2,28 @@
 const { setupEnvironmentFile } = require('./env-loader');
 setupEnvironmentFile();
 
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
 // Configuración específica para web
 config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
+
+// Add path aliases to support @/ imports in EAS Build
+config.resolver.extraNodeModules = {
+  '@': path.resolve(__dirname),
+  '@/components': path.resolve(__dirname, 'components'),
+  '@/constants': path.resolve(__dirname, 'constants'),
+  '@/types': path.resolve(__dirname, 'types'),
+  '@/utils': path.resolve(__dirname, 'utils'),
+  '@/services': path.resolve(__dirname, 'services'),
+  '@/hooks': path.resolve(__dirname, 'hooks'),
+  '@/assets': path.resolve(__dirname, 'assets'),
+  '@/dto': path.resolve(__dirname, 'dto'),
+  '@/models': path.resolve(__dirname, 'models'),
+  '@/environment': path.resolve(__dirname, 'environment'),
+  '@/contexts': path.resolve(__dirname, 'contexts'),
+};
 
 module.exports = config;
